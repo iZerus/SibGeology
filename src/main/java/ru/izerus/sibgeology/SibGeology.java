9c618796c540b1a235c5d1aab65d370a57ec294e
iZerus <pavelmi@list.ru>
2022-08-03 19:41:21 +0500

    Init new plugin

---
+++ src/main/java/ru/izerus/sibgeology/SibGeology.java
@@ -0,0 +1,14 @@
+package ru.izerus.sibgeology;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SibGeology extends JavaPlugin {
+	
+	static String message;
+    
+	@Override
+	public void onEnable() {
+
+	}
+	
+}
