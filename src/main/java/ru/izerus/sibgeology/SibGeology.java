29969e5f53e300dd16fbe7d72ddbd1b7e0e9051c
iZerus <pavelmi@list.ru>
2022-08-03 20:12:07 +0500

    feat: add config

--- src/main/java/ru/izerus/sibgeology/SibGeology.java
+++ src/main/java/ru/izerus/sibgeology/SibGeology.java
@@ -1,14 +1,39 @@
 package ru.izerus.sibgeology;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import ru.izerus.sibgeology.config.Config;
+
 public class SibGeology extends JavaPlugin {
-	
-	static String message;
-    
+
+	private static SibGeology instance;
+
+	public static SibGeology instance() {
+		return instance;
+	}
+
+	private Config config;
+
+	public Config config() {
+		return this.config;
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (command.getName().equalsIgnoreCase("sibgeology")) {
+			config.load();
+			sender.sendMessage("SibGeology reloaded: " + config.enabled());
+			return true;
+		}
+		return false;
+	}
+
 	@Override
 	public void onEnable() {
-
+		instance = this;
+		config = new Config(this);
 	}
-	
+
 }
---
+++ src/main/java/ru/izerus/sibgeology/config/Config.java
@@ -0,0 +1,52 @@
+package ru.izerus.sibgeology.config;
+
+import java.io.IOException;
+import java.util.logging.Level;
+
+import org.bukkit.configuration.file.FileConfiguration;
+
+import ru.izerus.sibgeology.SibGeology;
+
+public class Config {
+
+	public static final class Fields {
+		public static final String enabled = "enabled";
+	}
+
+	private final SibGeology plugin;
+
+	private boolean enabled;
+
+	public Config(SibGeology instance) {
+		plugin = instance;
+		plugin.saveDefaultConfig();
+		load();
+	}
+
+	public boolean enabled() {
+		return enabled;
+	}
+
+	public void load() {
+		plugin.reloadConfig();
+		FileConfiguration file = plugin.getConfig();
+
+		enabled = file.getBoolean(Fields.enabled);
+	}
+
+	public void save() {
+		FileConfiguration file = plugin.getConfig();
+
+		file.set(Fields.enabled, enabled);
+
+		String path = plugin.getDataFolder() + "/config.yml";
+		try {
+			file.save(path);
+		} catch (IOException e) {
+			this.plugin.getLogger().log(Level.WARNING, "Failed to save config", e);
+		}
+
+		load();
+	}
+
+}
